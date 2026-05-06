3354e31ae10e0ceba2224e251f5f88a2b35447b0
tiagoharry <tiagoassisandrade@hotmail.com>
2026-05-06 16:16:51 -0300

    finish project

---
+++ src/app/services/certificado.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Certificado } from '../interfaces/certificado';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CertificadoService {
+  certificados: Certificado[] = [];
+
+  constructor() { }
+
+  adicionarCertificado(certificado: Certificado) {
+    this.certificados.unshift({ ...certificado });
+    localStorage.setItem('certificados', JSON.stringify(this.certificados));
+  }
+
+  // obterCertificados() {
+  //   return this.certificados;
+  // }
+}
